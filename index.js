60232f6d1edee34f5e45180167cd3c87b5f31306
Tina Huynh Le <huynhletina@gmail.com>
2024-02-11 14:25:21 +0100

    add pete the baker solution

---
+++ index.js
@@ -0,0 +1,17 @@
+function cakes(recipe, available) {
+  // create an array numbers
+  // iterate over recipe
+  // for every key check the value of the same key
+  // in 'available' and divide the value from 'available' by the value of 'recipe'
+  // this number gets added to 'numbers'
+
+  // after the iteration we choose the lowest number of 'numbers' array and return it
+
+  const numbers = [];
+
+  for (let ingredient in recipe) {
+    if (!available[ingredient]) return 0;
+    numbers.push(Math.floor(available[ingredient] / recipe[ingredient]));
+  }
+  return Math.min(...numbers);
+}
