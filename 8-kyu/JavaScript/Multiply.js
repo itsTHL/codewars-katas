e9488bacd08e0808d824ceac2cdfe4f4e652e130
Tina Huynh Le <huynhletina@gmail.com>
2024-03-04 22:54:50 +0100

    add new kata

---
+++ 8-kyu/JavaScript/Multiply.js
@@ -0,0 +1,11 @@
+// Kata: https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+
+function multiply(a, b) {
+  a * b;
+}
+
+// Solution
+
+function multiply(a, b) {
+  return a * b;
+}
